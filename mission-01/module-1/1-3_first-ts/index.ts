4ecc7fc38edcd7fafa05bb67a051f145b7b12aae
rakibul-wdp <rakibul.wdp@gmail.com>
2021-01-11 04:05:00 +0600

    data type

--- mission-01/module-1/1-3_first-ts/index.ts
+++ mission-01/module-1/1-3_first-ts/index.ts
@@ -1,2 +1,22 @@
-let course: string = "hello";
-console.log(course);
+// Basic data type
+
+// string
+let firstName: string = "abul";
+
+// number
+let roll: number = 25;
+
+// boolean
+let isTrue: boolean = true;
+
+// undefined
+let x: undefined = undefined;
+
+// null
+let y: null = null;
+
+// tuple --> array --> order --> type of values
+let coordinates: [number, number] = [1, 2];
+
+let ageName: [number, string] = [25, "babul"];
+ageName[0] = 24;
