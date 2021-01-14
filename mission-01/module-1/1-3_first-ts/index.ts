fc74cdb4b8d5c783f979b23a57dab6d42afa249b
rakibul-wdp <rakibul.wdp@gmail.com>
2021-01-15 04:05:00 +0600

    reference and literal types data

--- mission-01/module-1/1-3_first-ts/index.ts
+++ mission-01/module-1/1-3_first-ts/index.ts
@@ -1,22 +1,16 @@
-// Basic data type
+// reference type -> Object
 
-// string
-let firstName: string = "abul";
-
-// number
-let roll: number = 25;
-
-// boolean
-let isTrue: boolean = true;
-
-// undefined
-let x: undefined = undefined;
-
-// null
-let y: null = null;
-
-// tuple --> array --> order --> type of values
-let coordinates: [number, number] = [1, 2];
-
-let ageName: [number, string] = [25, "babul"];
-ageName[0] = 24;
+const user: {
+  company: "sohoje"; // literal types
+  readonly designation: string;
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  isMarried: boolean;
+} = {
+  company: "sohoje",
+  designation: "developer",
+  firstName: "abul",
+  lastName: "cabul",
+  isMarried: false,
+};
