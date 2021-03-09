8edf9d0d67ea09bd9163afb85920412014fa3e3e
rakibul-wdp <rakibul.wdp@gmail.com>
2021-03-10 04:05:00 +0600

    type guard using instance of

--- mission-01/module-1/1-3_first-ts/index.ts
+++ mission-01/module-1/1-3_first-ts/index.ts
@@ -1,46 +1,59 @@
-// type guards
+// instanceof guard
 
-// typeof - type guard
+class Animal {
+  name: string;
+  species: string;
 
-type AlphaNumeric = string | number;
+  constructor(name: string, species: string) {
+    this.name = name;
+    this.species = species;
+  }
 
-const add = (param1: AlphaNumeric, param2: AlphaNumeric): AlphaNumeric => {
-  if (typeof param1 === "number" && typeof param2 === "number") {
-    return param1 + param2;
+  makeSound() {
+    console.log("sound");
+  }
+}
+
+class Dog extends Animal {
+  constructor(name: string, species: string) {
+    super(name, species);
+  }
+
+  makeBark() {
+    console.log("barking");
+  }
+}
+
+class Cat extends Animal {
+  constructor(name: string, species: string) {
+    super(name, species);
+  }
+
+  makeMew() {
+    console.log("mew");
+  }
+}
+
+// handle in smart way by using function
+
+const isDog = (animal: Animal): animal is Dog => {
+  return animal instanceof Dog;
+};
+const isCat = (animal: Animal): animal is Cat => {
+  return animal instanceof Cat;
+};
+
+const getAnimal = (animal: Animal) => {
+  if (isDog(animal)) {
+    animal.makeBark();
+  } else if (isCat(animal)) {
+    animal.makeMew();
   } else {
-    return param1.toString() + param2.toString();
+    animal.makeSound();
   }
 };
 
-const result1 = add("2", "3");
-console.log(result1);
+const dog = new Dog("dog", "dog");
+const cat = new Cat("cat", "cat");
 
-// in guard
-
-type NormalUser = {
-  name: string;
-};
-
-type AdminUser = {
-  name: string;
-  role: string;
-};
-
-const getUser = (user: NormalUser | AdminUser) => {
-  if ("role" in user) {
-    console.log(user.name, user.role);
-  } else {
-    console.log(user.name);
-  }
-};
-
-const normalUser: NormalUser = {
-  name: "normal x",
-};
-
-const adminUser: AdminUser = {
-  name: "admin y",
-  role: "admin",
-};
-
-getUser(adminUser);
+getAnimal(dog);
