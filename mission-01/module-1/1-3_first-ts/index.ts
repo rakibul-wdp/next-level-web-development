084fa0dadc8434ea181d45c70f91ae7e7d74bfe8
rakibul-wdp <rakibul.wdp@gmail.com>
2021-03-25 04:05:00 +0600

    abstraction in oop

--- mission-01/module-1/1-3_first-ts/index.ts
+++ mission-01/module-1/1-3_first-ts/index.ts
@@ -1,75 +1,49 @@
-// polymorphism
+// abstraction 1. interface 2. abstract
 
-class Person {
-  getSleep() {
-    console.log("sleeping 8 hours");
+// idea
+interface Vehicle1 {
+  startEngine(): void;
+  stopEngine(): void;
+  move(): void;
+}
+
+// real implementation
+class Car1 implements Vehicle1 {
+  startEngine(): void {
+    console.log("starting");
+  }
+  stopEngine(): void {
+    console.log("stop");
+  }
+  move(): void {
+    console.log("moving");
+  }
+  test() {
+    console.log("testing");
   }
 }
 
-class Student extends Person {
-  getSleep() {
-    console.log("sleeping 7 hours");
+const toyotaCar = new Car1();
+toyotaCar.startEngine();
+
+// abstract class
+abstract class Car2 {
+  abstract startEngine(): void;
+  abstract stopEngine(): void;
+  abstract move(): void;
+  test() {
+    console.log("testing");
   }
 }
 
-class Developer extends Person {
-  getSleep() {
-    console.log("sleeping 6 hours");
+class ToyotaCar extends Car2 {
+  startEngine(): void {
+    console.log("starting");
+  }
+  stopEngine(): void {
+    console.log("stop");
+  }
+  move(): void {
+    console.log("move");
   }
 }
-
-const getSleepingHours = (param: Person) => {
-  param.getSleep();
-};
-
-const person1 = new Person();
-const person2 = new Student();
-const person3 = new Developer();
-
-getSleepingHours(person1);
-getSleepingHours(person2);
-getSleepingHours(person3);
-
-class Shape {
-  getArea(): number {
-    return 0;
-  }
-}
-
-class Circle extends Shape {
-  radius: number;
-
-  constructor(radius: number) {
-    super();
-    this.radius = radius;
-  }
-
-  getArea(): number {
-    return Math.PI * this.radius * this.radius;
-  }
-}
-
-class Rectangle extends Shape {
-  height: number;
-  width: number;
-
-  constructor(height: number, width: number) {
-    super();
-    this.height = height;
-    this.width = width;
-  }
-
-  getArea(): number {
-    return this.height * this.width;
-  }
-}
-
-const getShapeArea = (param: Shape) => {
-  console.log(param.getArea());
-};
-
-const shape1 = new Shape();
-const shape2 = new Circle(10);
-const shape3 = new Rectangle(10, 5);
-
-getShapeArea(shape3);
