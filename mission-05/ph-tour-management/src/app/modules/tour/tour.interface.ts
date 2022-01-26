80e7e4fdbe8e244570bc660b3c34bf3bb4db88b9
rakibul-wdp <rakibul.wdp@gmail.com>
2022-01-27 04:05:00 +0600

    update tour interface

--- mission-05/ph-tour-management/src/app/modules/tour/tour.interface.ts
+++ mission-05/ph-tour-management/src/app/modules/tour/tour.interface.ts
@@ -3,20 +3,21 @@ import { Types } from "mongoose";
 export interface ITourType {
   name: string;
 }
-
 export interface ITour {
   title: string;
   slug: string;
   description?: string;
-  images: string[];
+  images?: string[];
   location?: string;
   costFrom?: number;
   startDate?: Date;
+  departureLocation?: string;
+  arrivalLocation?: string;
   endDate?: Date;
   included?: string[];
   excluded?: string[];
   amenities?: string[];
-  tourPlan?: string;
+  tourPlan?: string[];
   maxGuest?: number;
   minAge?: number;
   division: Types.ObjectId;
