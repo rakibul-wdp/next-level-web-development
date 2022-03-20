ce0887079e9d67cfa5d593e4b01b5350970fde98
rakibul-wdp <rakibul.wdp@gmail.com>
2022-03-21 04:05:00 +0600

    generate slug in update tour

--- mission-05/ph-tour-management/src/app/modules/tour/tour.service.ts
+++ mission-05/ph-tour-management/src/app/modules/tour/tour.service.ts
@@ -121,17 +121,17 @@ const updateTour = async (id: string, payload: Partial<ITour>) => {
     throw new Error("Tour not found.");
   }
 
-  // if (payload.title) {
-  //     const baseSlug = payload.title.toLowerCase().split(" ").join("-")
-  //     let slug = `${baseSlug}`
+  if (payload.title) {
+    const baseSlug = payload.title.toLowerCase().split(" ").join("-");
+    let slug = `${baseSlug}`;
 
-  //     let counter = 0;
-  //     while (await Tour.exists({ slug })) {
-  //         slug = `${slug}-${counter++}` // dhaka-division-2
-  //     }
+    let counter = 0;
+    while (await Tour.exists({ slug })) {
+      slug = `${slug}-${counter++}`; // dhaka-division-2
+    }
 
-  //     payload.slug = slug
-  // }
+    payload.slug = slug;
+  }
 
   const updatedTour = await Tour.findByIdAndUpdate(id, payload, { new: true });
 
