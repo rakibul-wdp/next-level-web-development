0ebcc179ff471bdc1164ad4ee1746a56b14069cf
rakibul-wdp <rakibul.wdp@gmail.com>
2025-05-04 07:43:47 +0600

    function with generics again

--- mission-01/module-1/1-3_first-ts/index.ts
+++ mission-01/module-1/1-3_first-ts/index.ts
@@ -1,10 +1,34 @@
-// constraints
+// function with generics
 
-const addCourseToStudent = <
-  T extends { id: number; name: string; email: string }
->(
-  student: T
-) => {
+const createArray = (param: string): string[] => {
+  return [param];
+};
+
+const createArrayWithGeneric = <T>(param: T): T[] => {
+  return [param];
+};
+
+const res1 = createArray("bangladesh");
+const resGeneric = createArrayWithGeneric<string>("bangladesh");
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const resGenericObj = createArrayWithGeneric<User>({ id: 222, name: "john" });
+
+const createArrayWithTuple = <T, Q>(param1: T, param2: Q): [T, Q] => {
+  return [param1, param2];
+};
+
+const res3 = createArrayWithTuple<string, number>("bangladesh", 33);
+const res4 = createArrayWithTuple<string, User>("bangladesh", {
+  id: 3,
+  name: "Uganda",
+});
+
+const addCourseToStudent = <T>(student: T) => {
   const course = "level 2";
 
   return {
@@ -12,28 +36,3 @@ const addCourseToStudent = <
     course,
   };
 };
-
-const student0 = addCourseToStudent({
-  id: 44,
-  name: "z",
-  email: "z@gmail",
-  not: "not",
-});
-
-const student1 = addCourseToStudent<{
-  id: number;
-  name: string;
-  email: string;
-  devType: string;
-}>({
-  id: 23,
-  name: "x",
-  email: "x@gmail.com",
-  devType: "l2",
-});
-const student2 = addCourseToStudent({
-  id: 33,
-  name: "y",
-  email: "y@gmail.com",
-  devType: "l2",
-});
