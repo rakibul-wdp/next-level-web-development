62af437bd79f3cb121065be731e6ca579addb342
rakibul-wdp <rakibul.wdp@gmail.com>
2025-05-05 08:27:19 +0600

    inheritance in oop

--- mission-01/module-1/1-3_first-ts/index.ts
+++ mission-01/module-1/1-3_first-ts/index.ts
@@ -1,19 +1,40 @@
-// oop - class
+// opp - inheritance
 
-class Animal {
-  // parameter properties
+class Person {
+  name: string;
+  age: number;
+  address: string;
 
-  constructor(
-    public name: string,
-    public species: string,
-    public sound: string
-  ) {}
+  constructor(name: string, age: number, address: string) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+  }
 
-  makeSound() {
-    console.log(this.name, this.sound);
+  getSleep(numOfHours: number) {
+    console.log(this.name, numOfHours);
   }
 }
 
-const dog = new Animal("german shepard", "dog", "ghew ghew");
-const cat = new Animal("asian cat", "cat", "mew mew");
-dog.makeSound();
+class Student extends Person {
+  constructor(name: string, age: number, address: string) {
+    super(name, age, address);
+  }
+}
+
+const student1 = new Student("x", 24, "ghana");
+
+class Teacher extends Person {
+  designation: string;
+
+  constructor(name: string, age: number, address: string, designation: string) {
+    super(name, age, address);
+    this.designation = designation;
+  }
+
+  takeClass(numOfHours: number) {
+    console.log(this.name, numOfHours);
+  }
+}
+
+const teacher1 = new Teacher("y", 54, "uganda", "lecturer");
