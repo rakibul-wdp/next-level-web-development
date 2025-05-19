e05216188ddf03c642cc8fac80fd97352adab79a
rakibul-wdp <rakibul.wdp@gmail.com>
2025-05-19 09:37:17 +0600

    behaviors during data deletion

---
+++ mission-02/foreignKey.sql
@@ -0,0 +1,43 @@
+CREATE TABLE "user"(
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(25) NOT NULL
+)
+
+CREATE TABLE post(
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  user_id INTEGER REFERENCES "user"(id) ON DELETE SET DEFAULT DEFAULT 2
+)
+
+ALTER TABLE post
+  ALTER COLUMN user_id SET NOT NULL;
+
+INSERT INTO "user" (username) VALUES
+('akash'),
+('batash'),
+('sagor'),
+('nodi');
+
+SELECT * from "user";
+
+INSERT INTO post (title, user_id) VALUES
+('Enjoying sunny data with Akash!', 2),
+('Batash just shared an amazing recipe!', 1),
+('Exploring adventures with Sagor', 4),
+('Nodi wisdom always leaves me inspired', 4);
+
+DROP TABLE post;
+DROP TABLE "user";
+
+SELECT * from post;
+
+INSERT INTO post (title, user_id) VALUES('test', NULL);
+
+-- Deletion constraint on DELETE user
+-- Cascading Deletion -> ON DELETE CASCADE
+-- Setting NULL -> ON DELETE SET NULL
+-- Restrict DEletion -> ON DELETE RESTRICT / ON DELETE NO ACTION (default)
+-- Set Default value -> ON DELETE SET DEFAULT
+
+DELETE FROM "user"
+  WHERE id = 4;
