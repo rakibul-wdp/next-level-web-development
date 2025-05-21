4d89257cea062713b8771624f78aa9e0d7b28498
rakibul-wdp <rakibul.wdp@gmail.com>
2025-05-21 09:18:21 +0600

    solve practice problem

--- mission-02/practice.sql
+++ mission-02/practice.sql
@@ -77,3 +77,30 @@ SELECT department_name, round(avg(salary)) as avg_salary FROM employees
 
 SELECT extract(YEAR FROM hire_date) as hire_year, count(*) FROM employees
   GROUP BY hire_year;
+
+CREATE TABLE orders (
+  order_id SERIAL PRIMARY KEY,
+  customer_id INT,
+  order_date DATE,
+  total_amount DECIMAL(10, 2)
+);
+
+INSERT INTO orders (customer_id, order_date, total_amount) VALUES
+  (1, '2022-01-05', 100.50),
+    (2, '2020-01-07', 200.75),
+    (1, '2022-01-08', 150.25),
+    (3, '2020-05-10', 300.00),
+    (2, '2022-01-15', 180.50),
+    (3, '2022-01-20', 220.25),
+    (1, '2022-01-25', 90.00),
+    (2, '2022-01-28', 120.75),
+    (3, '2021-02-01', 250.50),
+    (1, '2022-02-05', 180.25),
+    (4, '2023-02-05', 190.25);
+
+SELECT * FROM orders;
+SELECT customer_id, count(order_id), sum(total_amount) FROM orders GROUP BY customer_id HAVING count(order_id) > 2;
+
+SELECT * from orders;
+
+SELECT extract(month FROM order_date) as month, sum(total_amount) FROM orders WHERE extract(year FROM order_date) = 2022 GROUP BY month;
