dbb6f79f6c4d84404a8905716e01fb0a318cb08d
rakibul-wdp <rakibul.wdp@gmail.com>
2025-05-21 10:26:49 +0600

    views

--- mission-02/module10.md
+++ mission-02/module10.md
@@ -3,3 +3,11 @@
 A subquery is a nested query within another SQL statement.
 
 - Retrieve all employees whose salary is greater than the highest salary of the HR department
+
+#### View
+
+- Views are virtual tables generated from the result of a SQL query.
+- What is the purpose of SQL views?
+  1. Simplifying complex queries
+  2. Improved security
+  3. Enhanced data abstraction
---
+++ mission-02/views.sql
@@ -0,0 +1,19 @@
+SELECT * FROM employees;
+
+CREATE VIEW dept_avg_salary
+  AS SELECT department_name, avg(salary) FROM employees GROUP BY department_name;
+
+SELECT * FROM dept_avg_salary;
+
+CREATE VIEW test_view
+  AS
+    SELECT employee_name, salary, department_name
+  FROM employees
+    WHERE department_name in
+      (SELECT department_name FROM employees WHERE department_name LIKE '%R%');
+
+SELECT * FROM test_view;
+
+-- Simplifying complex queries
+-- Improved security
+-- Enhanced data abstraction
