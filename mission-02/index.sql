3ecba64664089f4dcca98368a3634c0060d1e29e
rakibul-wdp <rakibul.wdp@gmail.com>
2025-05-23 14:53:37 +0600

    index algo

--- mission-02/index.sql
+++ mission-02/index.sql
@@ -3,6 +3,9 @@ SELECT * FROM employees;
 EXPLAIN ANALYSE
 SELECT * FROM employees WHERE employee_name = 'Ava Wilson';
 
+EXPLAIN ANALYSE
+SELECT * FROM employees WHERE employee_id = 18;
+
 CREATE INDEX idx_employees_name
 ON employees (employee_name);
 
