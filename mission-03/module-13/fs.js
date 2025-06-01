3c1e978d5acef272c3bcfdaf600abe299109abba
rakibul-wdp <rakibul.wdp@gmail.com>
2025-06-01 07:04:07 +0600

    buffer

--- mission-03/module-13/fs.js
+++ mission-03/module-13/fs.js
@@ -2,27 +2,20 @@ const fs = require("fs");
 
 console.log("before");
 
-let text = "hello world";
-
-fs.writeFile("./hello.txt", text, { encoding: "utf-8" }, (err) => {
-  if (err) {
-    console.log("Something wrong", err);
-    return;
-  }
-
-  console.log("after writing ", data);
-});
-
 fs.readFile("./hello.txt", { encoding: "utf-8" }, (err, data) => {
   if (err) {
     console.log("Something went wrong", err);
     return;
   }
 
-  text = data;
-  console.log(text, "inside callback");
-});
+  fs.writeFile("./hello.txt", data, { encoding: "utf-8" }, (err) => {
+    if (err) {
+      console.log("Something wrong", err);
+      return;
+    }
 
-console.log(text);
+    console.log("after writing ", data);
+  });
+});
 
 console.log("after");
