3c42674d9395b341cf263d59c7c444d269bdf613
rakibul-wdp <rakibul.wdp@gmail.com>
2025-06-02 11:02:07 +0600

    set response headers

--- mission-03/module-13/todo-app/server.js
+++ mission-03/module-13/todo-app/server.js
@@ -1,8 +1,30 @@
 const http = require("http");
 
+const data = [
+  {
+    title: "prisma",
+    body: "learning node",
+    createdAt: "5/18/2025, 1: 25:02 AM",
+  },
+  {
+    title: "typescript",
+    body: "learning node",
+    createdAt: "5/18/2025, 1: 25:12 AM",
+  },
+];
+
 const server = http.createServer((req, res) => {
   if (req.url === "/todos" && req.method === "GET") {
-    res.end("All todos");
+    res.writeHead(200, {
+      // "content-type": "application/json",
+      "content-type": "text/html",
+      // email: "abul@gmail.com",
+    });
+    // res.setHeader("content-type", "text/plain");
+    // res.setHeader("email", "babul@gmail.com");
+    // res.statusCode = 201;
+    // res.end(JSON.stringify(data));
+    res.end(`<h1>hello world</h1> <h4>from the other</h4> <del>awesome</del>`);
   } else if (req.url === "/todos/create-todo" && req.method === "POST") {
     res.end("Todo created");
   } else {
