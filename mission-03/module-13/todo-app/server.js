81e6a5df7385b9cad37c1e8ff9c2dda1a7cb2f63
rakibul-wdp <rakibul.wdp@gmail.com>
2025-06-02 10:41:01 +0600

    routing in node js

--- mission-03/module-13/todo-app/server.js
+++ mission-03/module-13/todo-app/server.js
@@ -1,7 +1,13 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("welcome to todo app server");
+  if (req.url === "/todos" && req.method === "GET") {
+    res.end("All todos");
+  } else if (req.url === "/todos/create-todo" && req.method === "POST") {
+    res.end("Todo created");
+  } else {
+    res.end("route not found");
+  }
 });
 
 server.listen(5000, "127.0.0.1", () => {
