0859efead3a0fd3dd388359c22338dbe68b7ce4e
rakibul-wdp <rakibul.wdp@gmail.com>
2025-06-04 06:39:02 +0600

    explore req, res

--- mission-03/module-14/dist/app/app.js
+++ mission-03/module-14/dist/app/app.js
@@ -4,8 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const filePath = path_1.default.join(__dirname, "../../db/todo.json");
 app.get("/", (req, res) => {
     res.send("Hello World!!!");
+});
+app.get("/todos", (req, res) => {
+    const data = fs_1.default.readFileSync(filePath, { encoding: "utf-8" });
+    console.log(data);
+    res.json(data);
+});
+app.post("/todos/create-todo", (req, res) => {
+    const { title, body } = req.body;
+    console.log(title, body);
+    res.json({ title, body });
 });
 exports.default = app;
