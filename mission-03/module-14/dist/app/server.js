bde1b9fd8b8a828756876b9d842d5080a397e476
rakibul-wdp <rakibul.wdp@gmail.com>
2025-06-05 11:41:00 +0600

    connected to the mongodb

--- mission-03/module-14/dist/app/server.js
+++ mission-03/module-14/dist/app/server.js
@@ -12,10 +12,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongodb_1 = require("mongodb");
+const dotenv_1 = __importDefault(require("dotenv"));
 const app_1 = __importDefault(require("./app"));
 let server;
 const port = 5000;
+dotenv_1.default.config();
+const uri = process.env.MONGO_URI;
+const client = new mongodb_1.MongoClient(uri, {
+    serverApi: {
+        version: mongodb_1.ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
 const bootstrap = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield client.connect();
+    console.log("connected to mongodb");
     server = app_1.default.listen(port, () => {
         console.log(`Example app listening on port ${port}`);
     });
--- mission-03/module-14/src/app/server.ts
+++ mission-03/module-14/src/app/server.ts
@@ -1,9 +1,26 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
 import app from "./app";
 
 let server;
 const port = 5000;
 
+dotenv.config();
+
+const uri = process.env.MONGO_URI as string;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 const bootstrap = async () => {
+  await client.connect();
+  console.log("connected to mongodb");
+
   server = app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   });
