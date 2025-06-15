c5159c4438e48c63fd9cae1f29ceeb35818006f7
rakibul-wdp <rakibul.wdp@gmail.com>
2025-06-15 09:45:31 +0600

    setup mongoose

--- mission-03/module-17-note-app/src/server.ts
+++ mission-03/module-17-note-app/src/server.ts
@@ -1,12 +1,18 @@
 import { Server } from "http";
 import app from "./app";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 let server: Server;
 
 const PORT = 5000;
 
+dotenv.config();
+
 async function main() {
   try {
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("connected to mongodb using mongoose");
     server = app.listen(PORT, () => {
       console.log(`app listening on port ${PORT}`);
     });
