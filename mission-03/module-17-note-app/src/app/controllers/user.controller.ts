73c8c07bcb6e978bcaf4dfae32261a6eac6c2b9c
rakibul-wdp <rakibul.wdp@gmail.com>
2025-06-17 12:12:47 +0600

    create user controller

---
+++ mission-03/module-17-note-app/src/app/controllers/user.controller.ts
@@ -0,0 +1,83 @@
+import express, { Request, Response } from "express";
+import { User } from "../models/user.model";
+
+export const usersRoutes = express.Router();
+
+usersRoutes.post("/create-user", async (req: Request, res: Response) => {
+  const body = req.body;
+
+  const user = await User.create(body);
+
+  //   const myNote = new Note({
+  //     title: "Learning MongoDB",
+  //     tags: {
+  //       label: "database",
+  //     },
+  //   });
+
+  //   await myUser.save();
+
+  res.status(201).json({
+    success: true,
+    message: "user created",
+    user,
+  });
+});
+
+usersRoutes.get("", async (req: Request, res: Response) => {
+  const users = await User.find();
+
+  res.status(201).json({
+    success: true,
+    message: "note created",
+    users,
+  });
+});
+
+usersRoutes.get("/:userId", async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  //   const note = await User.findById(userId);
+  // const note = await User.findOne({ _id: userId });
+  const user = await User.findOne({ title: "Learning angular" });
+
+  res.status(201).json({
+    success: true,
+    message: "user created",
+    user,
+  });
+});
+
+usersRoutes.patch("/:userId", async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  const updatedBody = req.body;
+  // const user = await User.findByIdAndUpdate(userId, updatedBody, { new: true });
+  // const user = await User.updateOne({ _id: userId }, updatedBody, {
+  //   new: true,
+  // });
+  const user = await User.findOneAndUpdate({ _id: userId }, updatedBody, {
+    new: true,
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "user updated",
+    user,
+  });
+});
+
+usersRoutes.delete("/:userId", async (req: Request, res: Response) => {
+  const { userId } = req.params;
+  const user = await User.findByIdAndDelete(userId);
+  // const user = await User.updateOne({ _id: userId }, updatedBody, {
+  //   new: true,
+  // });
+  // const user = await User.findOneAndUpdate({ _id: userId }, updatedBody, {
+  //   new: true,
+  // });
+
+  res.status(201).json({
+    success: true,
+    message: "user deleted",
+    user,
+  });
+});
