0480b38ea79bbf9822d7334375dca4c16a1b5835
rakibul-wdp <rakibul.wdp@gmail.com>
2025-07-11 12:11:46 +0600

    separate main functionality of controller in service

--- mission-05/ph-tour-management/src/app/modules/user/user.controller.ts
+++ mission-05/ph-tour-management/src/app/modules/user/user.controller.ts
@@ -1,15 +1,10 @@
 import { Request, Response } from "express";
-import { User } from "./user.model";
 import httpStatus from "http-status-codes";
+import { UserServices } from "./user.service";
 
 const createUser = async (req: Request, res: Response) => {
   try {
-    const { name, email } = req.body;
-
-    const user = await User.create({
-      name,
-      email,
-    });
+    const user = await UserServices.createUser(req.body);
 
     res.status(httpStatus.CREATED).json({
       message: "user created successfully",
@@ -27,3 +22,5 @@ const createUser = async (req: Request, res: Response) => {
 export const UserControllers = {
   createUser,
 };
+
+// route matching -> controller -> service -> model -> DB
