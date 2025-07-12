2c7be6e7c8e89cd722f18d6360bd02fea6874de3
rakibul-wdp <rakibul.wdp@gmail.com>
2025-07-12 12:03:42 +0600

    create send response utility function

--- mission-05/ph-tour-management/src/app/modules/user/user.controller.ts
+++ mission-05/ph-tour-management/src/app/modules/user/user.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
 // const createUserFunction = async (req: Request, res: Response) => {
 //   const user = await UserServices.createUser(req.body);
@@ -25,21 +26,36 @@ const createUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserServices.createUser(req.body);
 
-    res.status(httpStatus.CREATED).json({
-      message: "user created successfully",
-      user,
+    // res.status(httpStatus.CREATED).json({
+    //   message: "user created successfully",
+    //   user,
+    // });
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "User created successfully",
+      data: user,
     });
   }
 );
 
 const getAllUsers = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const users = await UserServices.getAllUsers();
+    const result = await UserServices.getAllUsers();
 
-    res.status(httpStatus.OK).json({
+    // res.status(httpStatus.OK).json({
+    //   success: true,
+    //   message: "All Users Retrieved Successfully",
+    //   data: users,
+    // });
+
+    sendResponse(res, {
       success: true,
-      message: "All Users Retrieved Successfully",
-      data: users,
+      statusCode: httpStatus.CREATED,
+      message: "Users Retrieved successfully",
+      data: result.data,
+      meta: result.meta,
     });
   }
 );
