9807515b26a055a8e04c80b13c188c3c7cb349de
rakibul-wdp <rakibul.wdp@gmail.com>
2025-07-12 11:47:46 +0600

    create higher order component dry concept

--- mission-05/ph-tour-management/src/app/modules/user/user.controller.ts
+++ mission-05/ph-tour-management/src/app/modules/user/user.controller.ts
@@ -1,23 +1,52 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { UserServices } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
+// const createUserFunction = async (req: Request, res: Response) => {
+//   const user = await UserServices.createUser(req.body);
+
+//   res.status(httpStatus.CREATED).json({
+//     message: "user created successfully",
+//     user,
+//   });
+// };
+
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     createUserFunction(req, res);
+//   } catch (error) {
+//     console.log(error);
+//     next(error);
+//   }
+// };
+
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserServices.createUser(req.body);
 
     res.status(httpStatus.CREATED).json({
       message: "user created successfully",
       user,
     });
-  } catch (error) {
-    console.log(error);
-    next(error);
   }
-};
+);
+
+const getAllUsers = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const users = await UserServices.getAllUsers();
+
+    res.status(httpStatus.OK).json({
+      success: true,
+      message: "All Users Retrieved Successfully",
+      data: users,
+    });
+  }
+);
 
 export const UserControllers = {
   createUser,
+  getAllUsers,
 };
 
 // route matching -> controller -> service -> model -> DB
