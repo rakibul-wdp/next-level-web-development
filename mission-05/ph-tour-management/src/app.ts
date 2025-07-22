be9b37efcf8caccec4daffcb7c6e3dd496b8ae03
rakibul-wdp <rakibul.wdp@gmail.com>
2025-07-22 12:11:55 +0600

    setup google config

--- mission-05/ph-tour-management/src/app.ts
+++ mission-05/ph-tour-management/src/app.ts
@@ -4,9 +4,20 @@ import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import { router } from "./app/routes";
 import notFound from "./app/middlewares/notFound";
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import expressSession from "express-session";
 
 const app = express();
 
+app.use(
+  expressSession({
+    secret: "your secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
