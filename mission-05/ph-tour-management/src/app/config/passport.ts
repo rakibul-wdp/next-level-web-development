65af518b64577a08cb84fd16a345f07d25edae61
rakibul-wdp <rakibul.wdp@gmail.com>
2025-07-22 12:31:54 +0600

    complete google auth config setup with passport

--- mission-05/ph-tour-management/src/app/config/passport.ts
+++ mission-05/ph-tour-management/src/app/config/passport.ts
@@ -1,6 +1,12 @@
 import passport from "passport";
-import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import {
+  Strategy as GoogleStrategy,
+  Profile,
+  VerifyCallback,
+} from "passport-google-oauth20";
 import { envVars } from "./env";
+import { User } from "../modules/user/user.model";
+import { Role } from "../modules/user/user.interface";
 
 passport.use(
   new GoogleStrategy(
@@ -9,6 +15,48 @@ passport.use(
       clientSecret: envVars.GOOGLE_CLIENT_SECRET,
       callbackURL: envVars.GOOGLE_CALLBACK_URL,
     },
-    async () => {}
+    async (
+      accessToken: string,
+      refreshToken: string,
+      profile: Profile,
+      done: VerifyCallback
+    ) => {
+      try {
+        const email = profile.emails?.[0].value;
+
+        if (!email) {
+          return done(null, false, { message: "No email found" });
+        }
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+          user = await User.create({
+            email,
+            name: profile.displayName,
+            picture: profile.photos?.[0].value,
+            role: Role.USER,
+            isVerified: true,
+            auths: [
+              {
+                provider: "google",
+                providerId: profile.id,
+              },
+            ],
+          });
+        }
+
+        return done(null, user);
+      } catch (error) {
+        console.log("Google Strategy Error", error);
+        return done(error);
+      }
+    }
   )
 );
+
+// frontend localhost:5173 -> localhost:5000/api/v1/auth/google -> passport -> google OAuth consent -> gmail login -> successful -> callback url localhost:5000/api/v1/auth/google/callback -> db store -> token
+
+// Bridge == Google -> user db store -> token
+// Custom -> email, password, role: user, name... -> registration -> 1 user create
+// Google -> req -> google -> successful : Jwt Token : Role, email -> DB - Store -> token - api access
