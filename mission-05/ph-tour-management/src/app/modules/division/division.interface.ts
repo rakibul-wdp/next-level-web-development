7891150addc4c78fb36b6e37741f40217a0f28e9
rakibul-wdp <rakibul.wdp@gmail.com>
2025-07-25 12:03:52 +0600

    create division interface and model

---
+++ mission-05/ph-tour-management/src/app/modules/division/division.interface.ts
@@ -0,0 +1,18 @@
+export interface IDivision {
+  name: string;
+  slug: string;
+  thumbnail?: string;
+  description?: string;
+}
+
+/**
+ * division name = Chattogram Division
+ *
+ * slug = chattogram-division
+ *
+ * /:id => /389422309434
+ *
+ * /:id =>
+ *
+ * /:slug => /division/chattogram-division
+ */
