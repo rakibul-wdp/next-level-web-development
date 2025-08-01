1227919a2788625a71cbbbf157e3d78db21083db
rakibul-wdp <rakibul.wdp@gmail.com>
2025-08-01 12:22:17 +0600

    create booking routes

--- mission-05/ph-tour-management/src/app/routes/index.ts
+++ mission-05/ph-tour-management/src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { UserRoutes } from "../modules/user/user.route";
 import { AuthRoutes } from "../modules/auth/auth.route";
 import { DivisionRoutes } from "../modules/division/division.route";
 import { TourRoutes } from "../modules/tour/tour.route";
+import { BookingRoutes } from "../modules/booking/booking.route";
 
 export const router = Router();
 
@@ -22,6 +23,14 @@ const moduleRoutes = [
   {
     path: "/tour",
     route: TourRoutes,
+  },
+  {
+    path: "/booking",
+    route: BookingRoutes,
+  },
+  {
+    path: "/payment",
+    route: PaymentRoutes,
   },
 ];
 
