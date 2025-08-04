57eb58ee61227fbcfd73af838013c28a09a6978e
rakibul-wdp <rakibul.wdp@gmail.com>
2025-08-05 05:23:52 +0600

    create payment controller

--- mission-05/ph-tour-management/src/app/routes/index.ts
+++ mission-05/ph-tour-management/src/app/routes/index.ts
@@ -4,6 +4,7 @@ import { AuthRoutes } from "../modules/auth/auth.route";
 import { DivisionRoutes } from "../modules/division/division.route";
 import { TourRoutes } from "../modules/tour/tour.route";
 import { BookingRoutes } from "../modules/booking/booking.route";
+import { PaymentRoutes } from "../modules/payment/payment.route";
 
 export const router = Router();
 
