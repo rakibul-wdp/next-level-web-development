6eb707a23b2c79e849da49063bc7619f73b3e6d7
rakibul-wdp <rakibul.wdp@gmail.com>
2025-08-06 10:42:39 +0600

    update division route

--- mission-05/ph-tour-management/src/app/modules/division/division.route.ts
+++ mission-05/ph-tour-management/src/app/modules/division/division.route.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { multerUpload } from "../../config/multer.config";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { Role } from "../user/user.interface";
@@ -9,10 +10,18 @@ import {
 } from "./division.validation";
 
 const router = Router();
+/*
+ {
 
+ file : Image
+ data : body text data => req.body => req.body.data
+ }
+*/
+// Form data -> body, file
 router.post(
   "/create",
   checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  multerUpload.single("file"),
   validateRequest(createDivisionSchema),
   DivisionController.createDivision
 );
@@ -21,6 +30,7 @@ router.get("/:slug", DivisionController.getSingleDivision);
 router.patch(
   "/:id",
   checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+  multerUpload.single("file"),
   validateRequest(updateDivisionSchema),
   DivisionController.updateDivision
 );
